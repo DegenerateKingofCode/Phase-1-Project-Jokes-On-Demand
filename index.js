03a552d91f6f76ac7fd825ef8e8156560ba4807f
tannerleroy <tanner@tleroy.com>
2022-04-19 09:31:25 -0400

    starting to create if else for one-two part

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 init = () => {
-
+fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
+.then(res => res.json())
+.then(data => console.log(data))
 }
 
+if (data.type === "")
+
+
+// function oneOrTwoPartSorter (jokeObj){
+//     data.forEach(joke => {
+
+//     })
+
+
+
+// function initialJokeFiller (joke){
+
+// }
 
 document.addEventListener("DOMContentLoaded", init)
