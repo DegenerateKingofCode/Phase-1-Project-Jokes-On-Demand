c2ad79c021e197359273312ccbc626ad980d2fc9
tannerleroy <tanner@tleroy.com>
2022-04-19 15:19:45 -0400

    added an easter eggand fixed buttons

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 
 document.addEventListener("DOMContentLoaded", () => {
    fetchJoke(jokeUrl)
-    
+   fetchMeme(memeUrl)
     
     const allJokeBtn = document.getElementById("all-joke-filter")
     const safeModeBtn = document.getElementById("safe-mode-filter")
     const getJokeBtn = document.getElementById("new-joke-button")
+    const easterEgg = document.getElementById("easter-egg")
+
     getJokeBtn.addEventListener("click",() => fetchJoke(jokeUrl))
-    safeModeBtn.addEventListener("click", ()=> alert("safe mode enabled", safeMode))
-    allJokeBtn.addEventListener("click", ()=> alert("all jokes enabled",
-        makeAllJokesAvail))
+    safeModeBtn.addEventListener("click", ()=> {alert("safe mode enabled", jokeUrl = "https://v2.jokeapi.dev/joke/Any?safe-mode")})
+    allJokeBtn.addEventListener("click", ()=> {alert("caution: all jokes enabled"), jokeUrl = "https://v2.jokeapi.dev/joke/Any?"})
+    easterEgg.addEventListener("mouseover", ()=> alert("Thank you for checking out our page. We greatly appreciate the public APIs that made this possible"))
 
-    function makeAllJokesAvail(){
-        jokeUrl = "https://v2.jokeapi.dev/joke/Any?"
-        console.log(jokeUrl)}
 
-    function safeMode() {
-        jokeUrl = "https://v2.jokeapi.dev/joke/Any?safe-mode"
-        }
-
-        
 
 })
 // global variables
@@ -30,11 +24,29 @@ let memeUrl = "https://api.imgflip.com/get_memes"
 
 //callback functions 
 
+const fetchMeme = (memeUrl) => {
+    fetch(memeUrl)
+    .then(res => res.json())
+    .then(data => renderMemes(data))
+}
+
 const fetchJoke = (jokeUrl) => {
     fetch(jokeUrl)
     .then(res => res.json())
     .then(data => oneOrTwoParter(data))
 
+}
+const renderMemes = (returnFromFetch) => {
+    returnFromFetch.data.memes.forEach(meme => {
+        ul = document.getElementById("meme-list")
+        img = document.createElement("img")
+        memeName = document.getElementById("meme-name")
+        img.src = meme.url
+        img.width = 325
+        img.height = 350
+        
+        ul.append(img)
+    })
 }
 const oneOrTwoParter = (data) => {
 
@@ -53,6 +65,3 @@ if (data.type === "single"){
     lineTwo.textContent = data.delivery
     
 }
-
-
-
