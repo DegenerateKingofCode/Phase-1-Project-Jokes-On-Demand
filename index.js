5d9c0154755c33cb168dddea3c975da4fe0ce789
tannerleroy <tanner@tleroy.com>
2022-04-19 10:05:09 -0400

    working on JokeBox

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-init = () => {
-fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
-.then(res => res.json())
-.then(data => console.log(data))
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
+    .then(res => res.json())
+    .then(data => oneOrTwoParter(data))
+
+    // const allJokeBtn = document.getElementById("all-joke-filter")
+    // const safeModeBtn = document.getElementById("safe-mode-filter")
+    // const getJokeBtn = document.getElementById("new-joke-button")
+    // getJokeBtn.addEventListener("click",() => getNewJoke)
+
+})
+
+//callback functions 
+
+function oneOrTwoParter (data){
+
+const lineOne = document.getElementById("line-one")
+const lineTwo = document.getElementById("line-two")
+if (data.type === "single"){
+    lineOne.textContent = data.joke
+
+}else 
+    lineOne.textContent = data.setup
+    lineTwo.textContent = data.delivery
 }
 
-if (data.type === "")
 
-
-// function oneOrTwoPartSorter (jokeObj){
-//     data.forEach(joke => {
-
-//     })
-
-
-
-// function initialJokeFiller (joke){
+// function makeAllJokesAvail(){
 
 // }
 
-document.addEventListener("DOMContentLoaded", init)
+// const getNewJoke = () => {
+
+//  const lineOne = document.getElementById("line-one")
+//  const lineTwo = document.getElementById("line-two")
+
+// fetch("https://v2.jokeapi.dev/joke/Any?")
+// .then(res => res.json())
+// .then(data => oneOrTwoParter(data)
+
+// )}
+
+// function safeMode() {
