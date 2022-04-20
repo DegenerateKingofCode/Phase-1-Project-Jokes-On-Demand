8b06da5ff17e6d618eba7e9a5ca5ee67df4049f1
andrebroomes <tru3wreckz@gmail.com>
2022-04-20 13:37:11 -0400

    added spooky and programming buttons

--- index.js
+++ index.js
@@ -1,44 +1,75 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
-    .then(res => res.json())
-    .then(data => oneOrTwoParter(data))
-
-    // const allJokeBtn = document.getElementById("all-joke-filter")
-    // const safeModeBtn = document.getElementById("safe-mode-filter")
-    // const getJokeBtn = document.getElementById("new-joke-button")
-    // getJokeBtn.addEventListener("click",() => getNewJoke)
-
-})
-
-//callback functions 
-
-function oneOrTwoParter (data){
-
-const lineOne = document.getElementById("line-one")
-const lineTwo = document.getElementById("line-two")
-if (data.type === "single"){
-    lineOne.textContent = data.joke
-
-}else 
-    lineOne.textContent = data.setup
-    lineTwo.textContent = data.delivery
-}
-
-
-// function makeAllJokesAvail(){
-
-// }
-
-// const getNewJoke = () => {
-
-//  const lineOne = document.getElementById("line-one")
-//  const lineTwo = document.getElementById("line-two")
-
-// fetch("https://v2.jokeapi.dev/joke/Any?")
-// .then(res => res.json())
-// .then(data => oneOrTwoParter(data)
-
-// )}
-
-// function safeMode() {
+    fetchJoke(jokeUrl)
+    fetchMeme(memeUrl)
+     
+     const allJokeBtn = document.getElementById("all-joke-filter")
+     const safeModeBtn = document.getElementById("safe-mode-filter")
+     const getJokeBtn = document.getElementById("new-joke-button")
+     const easterEgg = document.getElementById("easter-egg")
+     const programming = document.getElementById("programming-filter")
+     const spooky = document.getElementById('spooky-filter')
+ 
+     getJokeBtn.addEventListener("click",() => fetchJoke(jokeUrl))
+     safeModeBtn.addEventListener("click", ()=> {alert("safe mode enabled", jokeUrl = "https://v2.jokeapi.dev/joke/Any?safe-mode")})
+     allJokeBtn.addEventListener("click", ()=> {alert("caution: all jokes enabled"), jokeUrl = "https://v2.jokeapi.dev/joke/Any?"})
+     easterEgg.addEventListener("mouseover", ()=> alert("Thank you for checking out our page. We greatly appreciate the public APIs that made this possible"))
+     programming.addEventListener('click', () => {alert("programming jokes enabled"), jokeUrl = 'https://v2.jokeapi.dev/joke/Programming'})
+     spooky.addEventListener('click', () => alert("Spooky Jokes Enabled"), jokeUrl="https://v2.jokeapi.dev/joke/Spooky")
+ 
+ })
+ // global variables
+ 
+ let jokeUrl = "https://v2.jokeapi.dev/joke/Any?safe-mode"
+ 
+ let memeUrl = "https://api.imgflip.com/get_memes"
+ 
+ //callback functions 
+ 
+ const fetchMeme = (memeUrl) => {
+     fetch(memeUrl)
+     .then(res => res.json())
+     .then(data => renderMemes(data))
+ }
+ 
+ const fetchJoke = (jokeUrl) => {
+     fetch(jokeUrl)
+     .then(res => res.json())
+     .then(data => oneOrTwoParter(data))
+ 
+ }
+ const renderMemes = (returnFromFetch) => {
+     returnFromFetch.data.memes.forEach(meme => {
+         ul = document.getElementById("meme-list")
+         img = document.createElement("img")
+         img.src = meme.url
+         img.width = 350
+         img.height = 350
+         
+         ul.append(img)
+     })
+ }
+ const oneOrTwoParter = (data) => {
+ 
+ const lineOne = document.getElementById("line-one")
+ const lineTwo = document.getElementById("line-two")
+ let categoryLine = document.getElementById("category")
+ categoryLine.textContent = `Catergory: ${data.category}`
+ 
+ if (data.type === "single"){
+     lineOne.textContent = data.joke
+ 
+ }else 
+     lineOne.textContent = data.setup
+     lineTwo.textContent = data.delivery
+ }
+//  const form = document.getElementById('form').addEventListener('submit', function(e){
+//      e.preventDefault()
+//      fetch()
+//      const lineOne = document.getElementById("line-one").value
+//      const lineTwo = document.getElementById("line-two").value
+//      let categoryLine = document.getElementById("category").value
+  
+//     const newJoke = {lineOne, lineTwo, categoryLine}
+//     oneOrTwoParter(newJoke)
+//  })
